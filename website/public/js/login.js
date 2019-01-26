c450db42846086005e1b3301bbab3cf4c60ce4c0
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2019-01-26 17:14:20 -0600

    Login feature

---
+++ website/public/js/login.js
@@ -0,0 +1,49 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    console.log(user);
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+    var id = firebase.auth().id;
+    if(user != null){
+
+      var email_id = user.email;
+      console.log(currentUser);
+      console.log(user);
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      //var id = user.
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+  //var id = document.getElementById("User_UID").value;
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    
+    window.alert("Error : " + errorMessage);
+  });
+
+}
+
+/*function getUsrId(){
+  firebase.auth().
+}*/
+
+function logout(){
+  firebase.auth().signOut();
+}
