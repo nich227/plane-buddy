f0ccfb5ef96413bdc2916d181552c4b4916933e2
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2019-01-26 22:36:22 -0600

    Added push and pull from firebase

--- website/public/js/login.js
+++ website/public/js/login.js
@@ -1,38 +1,65 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
-    console.log(user);
     document.getElementById("user_div").style.display = "block";
     document.getElementById("login_div").style.display = "none";
-
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user.uid);
+        var uid = user.uid; //variable uid, has uid to get data base related file UID
+        console.log(uid);
+        var database = firebase.database();
+        var ref = database.ref('users');
+        ref.on('value', gotData, errData);
+        var data = { //set data to push, get from usr choice
+          usr: uid, 
+          resp: "Babies"
+        }
+        console.log(ref.push(data)); 
+        //console.log(data.val);
+       // var ref = database.ref('users');
+        //ref.on('value', gotData, errData);
+      }
+    });
     var user = firebase.auth().currentUser;
     var id = firebase.auth().id;
     if(user != null){
 
       var email_id = user.email;
-      console.log(currentUser);
-      console.log(user);
       document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
-      //var id = user.
-
-      document.getElementById("login_status").innerHTML = "Sign out";
     }
 
   } else {
     // No user is signed in.
-
     document.getElementById("user_div").style.display = "none";
     document.getElementById("login_div").style.display = "block";
 
-    document.getElementById("login_status").innerHTML = "Sign in";
   }
 });
+
+function gotData(data){//get data from firebase
+  console.log(data.val());
+  var usr = data.val();
+  var keys = Object.keys(usr);
+  console.log(keys);
+  for(var i = 0; i < keys.length; i++){
+    var k = keys[i];
+    var user = usr[k].usr;
+    var resp = usr[k].resp;
+    console.log(user, resp);
+  }
+}
+
+function errData(err){
+  console.log("Error");
+  console.log(err);
+}
+
 
 function login(){
 
   var userEmail = document.getElementById("email_field").value;
   var userPass = document.getElementById("password_field").value;
-  //var id = document.getElementById("User_UID").value;
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -47,12 +74,6 @@ function login(){
   firebase.auth().
 }*/
 
-
 function logout(){
   firebase.auth().signOut();
-}
-
-
-function continueToMatching(){
-  window.location = "matching.html";
 }
