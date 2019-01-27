92e61607c8ea9acbccaecfb1da2344c78513a832
Kevin Chen <ningchen227@gmail.com>
2019-01-27 02:32:59 -0600

    Change how pushing data works

--- website/public/js/matching.js
+++ website/public/js/matching.js
@@ -5,7 +5,7 @@ let acces = 'i';
 function valid_options(ba, sl, ac) {
     var newNode = document.createElement("P");
     var textNode = document.createTextNode("The option(s) you chose are invalid");
-    if(ba == 'i' || ac == 'i' || sl == 'i')
+    if(ba == 'i' && ac == 'i' && sl == 'i')
     {
         newNode.appendChild(textNode);
         document.getElementById("submit-area").appendChild(newNode);
@@ -15,6 +15,13 @@ function valid_options(ba, sl, ac) {
         baby = ba;
         social = sl;
         acces = ac;
+        
+        if(baby != 'i')
+            pushData(baby);
+        else if(social != 'i')
+            pushData(social);
+        else if(acces != 'i')
+            pushData(acces);
         window.location = "seat_chooser.html";
     }
         
