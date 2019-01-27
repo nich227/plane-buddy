7e5f25dd78ca2dadc668d6adafec7e1eedc0618d
Dylan Kreth <dylankreth@gmail.com>
2019-01-26 18:15:51 -0600

    initial file for placing people in seats

---
+++ seatPlacer.js
@@ -0,0 +1,12 @@
+
+function () {
+    let users = []; //get users fromm database
+    let nextSeatNumber = 0;
+    let seats = [];//array to fill with users that have been assigned to seats
+    let done = false;
+    //place all ada users
+    //place all sleeping users
+    //place all reading users
+    //place all talkers
+
+}
