e0a3ef1355f5278bc1050b279021e5408146e466
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2019-01-27 07:39:27 -0600

    login fix

--- website/public/js/matching.js
+++ website/public/js/matching.js
@@ -15,15 +15,29 @@ function valid_options(ba, sl, ac) {
         baby = ba;
         social = sl;
         acces = ac;
-        /*
+        console.log("HERE BIT" + uid);
         if(baby != 'i')
             pushData(baby);
         else if(social != 'i')
             pushData(social);
         else if(acces != 'i')
             pushData(acces);
-            */
+            console.log("here"); 
         window.location = "seat_chooser.html";
+        console.log("here");
     }
         
 }
+
+/*
+pushData(str){
+    var database = firebase.database();
+    var ref = database.ref('users');
+    ref.on('value', gotData, errData);
+    //console.log(arr);
+    var data = { //set data to push, get from usr choice
+      usr: uid, 
+      resp: "test"//actual response go here
+    }
+    console.log(ref.push(data)); 
+}*/
